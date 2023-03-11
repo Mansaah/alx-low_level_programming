046ba1ae84d69c13b688946776a00ec0888645e1
Mansaah <isahmusa61@gmail.com>
2023-03-11 10:59:27 -0800

    program that prints the minimum number of coins to make change for an amount of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,72 +1,78 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
+int check_change(int amount);
 /**
- * coinConverter - Helper function that does all the mathematics
- * @i: Passed in variable from main for calculations
- * Return: The number of coins needed minimum for the passed in variable
- */
-int coinConverter(int i)
-{
-	int count = 0;
-
-	while (i != 0)
-	{
-		if (i % 10 == 9 || i % 10 == 7)
-			i -= 2;
-		else if (i % 25 == 0)
-			i -= 25;
-		else if (i % 10 == 0)
-			i -= 10;
-		else if (i % 5 == 0)
-			i -= 5;
-		else if (i % 2 == 0)
-		{
-			if (i % 10 == 6)
-				i -= 1;
-			else
-				i -= 2;
-		}
-		else
-			i -= 1;
-
-		count++;
-	}
-
-	return (count);
-}
-
-/**
- * main - Takes in exactly one argument for minimum coin count
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
+ * main - prints the minimum number of coins to make change
+ * for an amount of money
+ * @argc: number of arguments
+ * @argv: array of argumnets
+ *
+ * Return: 0 if successful and 1 otherwise
  */
 int main(int argc, char *argv[])
 {
-	int i, coin;
-
-	coin = 0;
+	int amount, count = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	i = atoi(argv[1]);
-
-	if (i < 0)
-		printf("0\n");
-
 	else
 	{
-		coin = coinConverter(i);
+		amount = atoi(argv[1]);
 
-		printf("%d\n", coin);
+		if (amount < 0)
+		{
+			printf("%d\n", count);
+		}
+		else
+		{
+			count = check_change(amount);
+			printf("%d\n", count);
+		}
+
+		return (0);
+	}
+}
+/**
+ * check_change - checks for the minum possible coins
+ * @amount: amount to check
+ *
+ * Return: number of possible coins
+ */
+int check_change(int amount)
+{
+	int count = 0;
+
+	while (amount != 0)
+	{
+		if (amount - 25 >= 0)
+		{
+			amount -= 25;
+			count++;
+		}
+		else if (amount - 10 >= 0)
+		{
+			amount -= 10;
+			count++;
+		}
+		else if (amount - 5 >= 0)
+		{
+			amount -= 5;
+			count++;
+		}
+		else if (amount - 2 >= 0)
+		{
+			amount -= 2;
+			count++;
+		}
+		else
+		{
+			amount -= 1;
+			count++;
+		}
 	}
 
-	return (0);
+	return (count);
 }
