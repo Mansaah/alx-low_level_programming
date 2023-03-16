a24d37ea0312819796ddfff1445d13e5925bb778
Mansaah <isahmusa61@gmail.com>
2023-03-16 16:36:03 -0700

    memory allocation

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void *malloc_checked(unsigned int b);
+int _putchar(char c);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+#endif
