c91e201eb74d300eb34338ca76a86c778d0bac20
Mansaah <isahmusa61@gmail.com>
2023-03-24 09:31:43 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,2 +1,19 @@
-#include "function_pointers.h" /** * array_iterator - executes a function in all elements of an array * @array: array * @size: array size * @action: action performed on array elements */void array_iterator(int *array, size_t size, void (*action)(int)){	size_t i; 	if (size > 0 && action && array)	{		for (i = 0; i < size; i++)		{			action(array[i]);		}	}
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a function in all elements of an array
+ * @array: array
+ * @size: array size
+ * @action: action performed on array elements
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (size > 0 && action && array)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
 }
