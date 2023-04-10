a73d0faa9e673b4c6590e5334b778575f096fd90
Mansaah <isahmusa61@gmail.com>
2023-04-10 01:18:12 -0700

    my commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,25 +9,28 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	long int r;
+	int nletters;
+	int rwr;
 
 	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd < 0)
+	if (fd == -1)
 		return (-1);
 
 	if (text_content)
-	{
-		r = write(fd, text_content, strlen(text_content));
-		if (r < 0)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
-	close(fd);
+		text_content = "";
 
-	return (1);
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
+
+		close(fd);
+
+		return (1);
 }
